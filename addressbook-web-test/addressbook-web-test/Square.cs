0b0913462228bcbdf31117140e7467b7601a2efb
Александр Вуколов <hellocawka@gmail.com>
2019-11-04 20:05:18 +0300

    Задание №3: Создать вспомогательные методы и классы

---
+++ addressbook-web-test/addressbook-web-test/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_test
+{
+    class Square : Figure
+    {
+        private int size;
+        public Square(int size)
+        {
+            this.Size = size;
+        }
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+    }
+}
